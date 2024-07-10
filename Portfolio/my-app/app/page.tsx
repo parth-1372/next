aef4f7fb60236b3ccda7b7f27af2be1cf9706e38
Parth <parthmungra1372@gmail.com>
2024-07-10 21:49:09 +0530

    Project Almost Complete

--- Portfolio/my-app/app/page.tsx
+++ Portfolio/my-app/app/page.tsx
@@ -5,22 +5,27 @@ import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 import { Grid } from "@react-three/drei";
 import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/Data";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
     <main className=" flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 bg-black-100 text-white">
       <div className="max-w-7xl w-full">
        <FloatingNav
-       navItems={[
-        {name:'Home',
-         link:'/',
-         icon: <FaHome/>
-        }
-       ]}
+       navItems={navItems}
        />
        <Hero/>
        {/* <Grid/> */}
        <RecentProjects/>
+       <Clients/>
+       <Experience/>
+       <Approach/>
+       <Footer/>
       </div>
     </main>
     
---
+++ Portfolio/my-app/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://61dbc852a98c76f87ac7f955384bf3dd@o4507575859085312.ingest.us.sentry.io/4507575861575680",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
